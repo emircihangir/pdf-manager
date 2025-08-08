eda1b61eeaaa8c615b73225c96d3f40638b97cbc
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2025-08-08 13:39:49 +0300

    Create InputIssue class

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
+from enum import Enum
+import re
+
+class InputIssue(Enum):
+    UNCLOSED_PARENTHESES = "Unclosed parentheses detected in the range input."
+    NO_DASH_PRESENT = "No dash is present between parentheses."
+    EMPTY_INPUT = "Please provide a range input."
 
 # region global variables
 selected_file: str | None = None
